4cb3672da0aeb70b5006c35dadb4cf845ce13a61
zhangxiesen <zhangxiesen@tju.edu.cn>
2026-09-19 14:45:24 +0800

    move_method的java_output

--- java_output/SourceClass1.java
+++ java_output/SourceClass1.java
@@ -1,61 +1,27 @@
-package test.inline;
+package test.move;
 
-// Source class with the method to be inlined
 public class SourceClass1 {
-    public void execute() {
-        HelperClass helper = new HelperClass();
-        helper.performTask();
+
+    public void methodToBeMoved(TargetClass1 target) {
+        System.out.println("Method from SourceClass1, called with parameter: " + target.getInfo());
+    }
+
+    // Nested class in SourceClass1
+    class NestedClass {
+        public void callMovedMethod(TargetClass1 target) {
+            methodToBeMoved(target);
+        }
     }
 }
 
-// Helper class with private method that will be inlined
-class HelperClass {
-    public void performTask() {
-        String result = methodToBeInlined();
-        System.out.println(result);
+class TargetClass1 {
+    private String info;
+
+    public TargetClass1(String info) {
+        this.info = info;
     }
 
-    private String methodToBeInlined() {
-        return "Method Inlined!";
-    }
-}
-
-// Composition class accessing the HelperClass
-class CompositionClass {
-    private HelperClass helper;
-
-    public CompositionClass() {
-        this.helper = new HelperClass();
-    }
-
-    public void execute() {
-        helper.performTask();
-    }
-}
-
-// Superclass with a method to be invoked using super
-class SuperClass {
-    protected void displayMessage() {
-        System.out.println("Message from SuperClass");
-    }
-}
-
-// Subclass invoking superclass method using super keyword
-class SubClass extends SuperClass {
-    @Override
-    protected void displayMessage() {
-        super.displayMessage();
-        System.out.println("Message from SubClass");
-    }
-}
-
-// Factory class with private constructor and static factory method
-class Factory {
-    private Factory() {
-        // Private constructor
-    }
-
-    public static Factory createInstance() {
-        return new Factory();
+    public String getInfo() {
+        return info;
     }
 }
